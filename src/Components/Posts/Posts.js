2262a27ff5715820817d476b07233a167b43b4d6
Asif Muhammed <asifasifpsps@gmail.com>
2023-07-27 19:54:06 +0530

    space fix 4

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,29 +1,32 @@
-import React,{useState,useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from "react";
 
-
-import Heart from '../../assets/Heart';
-import { FirebaseContext } from '../../store/Context';
-import { PostContext } from '../../store/PostContext';
-import './Post.css';
-import {useHistory} from 'react-router-dom'
+import Heart from "../../assets/Heart";
+import { FirebaseContext } from "../../store/Context";
+import { PostContext } from "../../store/PostContext";
+import "./Post.css";
+import { useHistory } from "react-router-dom";
 
 function Posts() {
- const{firebase} = useContext(FirebaseContext)
-const [products,setProducts] = useState([])
-const {setPostDetails} = useContext(PostContext)
-const history = useHistory()
+  const { firebase } = useContext(FirebaseContext);
+  const [products, setProducts] = useState([]);
+  const { setPostDetails } = useContext(PostContext);
+  const history = useHistory();
 
-useEffect(()=>{
-  firebase.firestore().collection('products').get().then((snapshot)=>{
-    const allPost =  snapshot.docs.map((product)=>{
-      return {
-        ...product.data(),
-        id:product.id
-      }
-    })
-   setProducts(allPost)
-  })
-})
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then((snapshot) => {
+        const allPost = snapshot.docs.map((product) => {
+          return {
+            ...product.data(),
+            id: product.id,
+          };
+        });
+        setProducts(allPost);
+      });
+  });
 
   return (
     <div className="postParentDiv">
@@ -33,38 +36,32 @@ useEffect(()=>{
           <span>View more</span>
         </div>
         <div className="cards">
-          
-          { products.map(product=>{
-
-         return <div
-            className="card"
-            onClick={()=>{
-              setPostDetails(product)
-              history.push('/view')
-
-            }}
-          >
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src={product.url} alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; {product.price}</p>
-              <span className="kilometer">{product.category}</span>
-              <p className="name"> {product.name}</p>
-            </div>
-            <div className="date">
-              <span>{product.createdAt}</span>
-            </div>
-          </div>
-          })
-}
-
-
-
-
+          {products.map((product) => {
+            return (
+              <div
+                className="card"
+                onClick={() => {
+                  setPostDetails(product);
+                  history.push("/view");
+                }}
+              >
+                <div className="favorite">
+                  <Heart></Heart>
+                </div>
+                <div className="image">
+                  <img src={product.url} alt="" />
+                </div>
+                <div className="content">
+                  <p className="rate">&#x20B9; {product.price}</p>
+                  <span className="kilometer">{product.category}</span>
+                  <p className="name"> {product.name}</p>
+                </div>
+                <div className="date">
+                  <span>{product.createdAt}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="recommendations">
